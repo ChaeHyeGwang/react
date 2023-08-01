a6086ee0c1dbc7653835ab11e5bb0c088fc3a8df
SeungHyeon Cho <elk1219@naver.com>
2023-08-01 20:16:18 +0900

    React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+export default function App(){
+  var response1 = "";
+  var response2 = "";
+  var response3 = "";
+
+  axios.get("http://localhost:3002/test/get")
+       .then(async function (response) { response1 = response.data; console.log(response1); })
+       .catch(function (error) { console.log("ERROR : " + error); })
+       .then(function (response) {  });
+
+  axios.post("http://localhost:3002/test/post")
+       .then(async function (response) { response2 = response.data; console.log(response2); })
+       .catch(function (error) { console.log("ERROR : " + error); })
+       .then(function (response) {  });
+
+  axios.get("http://localhost:3002/test/db")
+       .then(async function (response) { response3 = response.data; console.log(response3); })
+       .catch(function (error) { console.log("ERROR : " + error); })
+       .then(function (response) {  });
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> 팀장님 힘내십쇼
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -21,5 +41,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
